abd0b6b7db3a7c23a74b2f3098c94c3ddffd5ba5
parthkeskar05-byte <parthkeskar05@gmail.com>
2026-05-23 13:32:15 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from langchain_groq import ChatGroq
-from langchain_openai import ChatOpenAI
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
