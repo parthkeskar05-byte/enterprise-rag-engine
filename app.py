087fede8b9e6ec85fa1b5054068c22f9d8cc5a18
parthkeskar05-byte <parthkeskar05@gmail.com>
2026-05-23 13:46:37 +0530

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_huggingface import HuggingFaceEndpoint
+
 from langchain_classic.chains import RetrievalQA
 import os
 import tempfile
